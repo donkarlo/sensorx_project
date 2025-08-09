872711803f31f71d8ebe92b448df97722a0f707d
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-08-09 23:05:54 +0200

    estimation and state updates

--- impl/boot_for_two_robots_and_two_sensors.py
+++
@@ -1,5 +0,0 @@
-from sensorx.obs.sensor_set_obss_from_yaml_file import SensorSetObsSeqFromYamlFile
-
-class BootForTwoRobotsAndTwoSensors:
-    def __generate_sensor_set_time_val_seq(self):
-        self._topics_yaml_file = SensorSetObsSeqFromYamlFile()
--- src/sensorx/state/state.py
+++
@@ -1,9 +0,0 @@
-from mathx.linalg.matrix.vec.col_vec import ColVec
-
-
-class State:
-    '''
-    This version talks only about robots. Later we can have something from cntrol theory
-    '''
-    def __init__(self, vec:ColVec):
-        self._vec:ColVec = vec
